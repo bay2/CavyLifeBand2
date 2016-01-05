5d1fa6caccdbb983c9476c502584b24ca7d3dc27
bay2 <382499488@qq.com>
2016-01-05 14:26:49 +0800

    first commit

---
+++ CavyLifeBand2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CavyLifeBand2
+//
+//  Created by xuemincai on 16/1/5.
+//  Copyright © 2016年 xuemincai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :ios, "8.0"
+use_frameworks!
+
+pod 'SnapKit'
+pod 'Alamofire'
+pod 'AlamofireImage'
+pod 'AlamofireObjectMapper'
+pod 'Log'
+pod 'XLActionController'
+pod 'SwiftOverlays'
+pod 'EZSwiftExtensions'
+pod 'MJRefresh'
