5242a8f88e82c755bf85e39448ea2828bd8b7112
bay2 <382499488@qq.com>
2016-01-06 11:15:03 +0800

    代码结构提交

---
+++ CavyLifeBand2/Common/CavyLifeBand2-Define.swift
@@ -0,0 +1,11 @@
+//
+//  CavyLifeBand2-Define.swift
+//  CavyLifeBand2
+//
+//  Created by xuemincai on 16/1/6.
+//  Copyright © 2016年 xuemincai. All rights reserved.
+//
+
+import Foundation
+
+let bugHDKey = "64dcd8aad631f17fd3ee1eb8aad1c08e"
---
+++ CavyLifeBand2/Common/CavyLifeBand2-OC.h
@@ -0,0 +1,14 @@
+//
+//  CavyLifeBand2-OC.h
+//  CavyLifeBand2
+//
+//  Created by xuemincai on 16/1/6.
+//  Copyright © 2016年 xuemincai. All rights reserved.
+//
+
+#ifndef CavyLifeBand2_OC_h
+#define CavyLifeBand2_OC_h
+
+#import <BugHD/BugHD.h> 
+
+#endif /* CavyLifeBand2_OC_h */
