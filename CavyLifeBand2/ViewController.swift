56c824d6e7cba98cfd624884334f6a79d532cdf2
bay2 <382499488@qq.com>
2016-01-07 13:47:17 +0800

    测试BugHD

--- CavyLifeBand2/ViewController.swift
+++ CavyLifeBand2/ViewController.swift
@@ -20,6 +20,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func aa(sender: UIButton) {
+        
+        var aaa: String?
+        
+        print("\(aaa!)")
+        
+    }
 
 }
 
