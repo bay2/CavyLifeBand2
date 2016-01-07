dd4843fa9f3f718143fd5d290793cbd1b2f2d0cb
bay2 <382499488@qq.com>
2016-01-07 10:03:25 +0800

    删除无用文件

--- build/Release-iphoneos/Log.framework/Headers/Log-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "Log.h"
-
-FOUNDATION_EXPORT double LogVersionNumber;
-FOUNDATION_EXPORT const unsigned char LogVersionString[];
-
--- build/Release-iphoneos/SwiftOverlays.framework/Headers/SwiftOverlays-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double SwiftOverlaysVersionNumber;
-FOUNDATION_EXPORT const unsigned char SwiftOverlaysVersionString[];
-
