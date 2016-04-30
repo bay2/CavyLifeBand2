b275c6c3b583032b122714b7ffa7887fec3ccc91
蔡学敏 <382499488@qq.com>
2016-04-30 08:02:55 +0800

    修改编译配置

--- uitest.sh
+++ uitest.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 xcodebuild clean build test -workspace $APP_NAME.xcworkspace -scheme $APP_NAME"UITests" -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.2' OTHER_SWIFT_FLAGS=" -D UITEST"  | xcpretty
+bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"
