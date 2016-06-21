b318573678c2eae4a1a031314fe77183618022f1
JLCavy <JL@JL.local>
2016-06-21 16:59:16 +0800

    成就界面是否可滑动根据屏幕宽度决定

--- CavyLifeBand2/Contacts/View/UserAchievementView.swift
+++ CavyLifeBand2/Contacts/View/UserAchievementView.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import RealmSwift
+import EZSwiftExtensions
 
 class UserAchievementView: UIView, UserInfoRealmOperateDelegate, ChartsRealmProtocol {
 
@@ -81,8 +82,8 @@ class UserAchievementView: UIView, UserInfoRealmOperateDelegate, ChartsRealmProt
         // 成就图标展示视图设置
         collectionView.delegate   = self
         collectionView.dataSource = self
-        collectionView.scrollEnabled = false 
-        collectionView.showsVerticalScrollIndicator = false
+        collectionView.scrollEnabled = false
+        collectionView.showsVerticalScrollIndicator = ez.screenWidth <= 320 ? true : false
         collectionView.registerNib(UINib(nibName: userAchievementViewCollectionCell, bundle: nil), forCellWithReuseIdentifier: userAchievementViewCollectionCell)
         
         self.setCornerRadius(radius: CavyDefine.commonCornerRadius)
