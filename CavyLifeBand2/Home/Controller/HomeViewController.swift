a25dad4211bc88dfbdc20b73f3c5b494773a7ce4
Jessica <Jessica@JessicadeiMac.local>
2016-06-28 17:17:52 +0800

    修改接口后更改主页逻辑

--- CavyLifeBand2/Home/Controller/HomeViewController.swift
+++ CavyLifeBand2/Home/Controller/HomeViewController.swift
@@ -20,7 +20,7 @@ let dateViewHeight: CGFloat = 50.0
 let ringViewHeight: CGFloat = 96 + ez.screenWidth * 0.55
 let navBarHeight: CGFloat = 64.0
 
-class HomeViewController: UIViewController, BaseViewControllerPresenter, ChartsRealmProtocol, HomeListRealmProtocol, SinglePKRealmModelOperateDelegate {
+class HomeViewController: UIViewController, BaseViewControllerPresenter, ChartsRealmProtocol,  SinglePKRealmModelOperateDelegate {
     
     var leftBtn: UIButton? = {
         
@@ -96,7 +96,6 @@ class HomeViewController: UIViewController, BaseViewControllerPresenter, ChartsR
  
     }
 
-    
     /**
      手环断线通知
      
@@ -236,147 +235,7 @@ class HomeViewController: UIViewController, BaseViewControllerPresenter, ChartsR
         
     }
     
-    // MARK: --- 解析数据 保存数据库
-    
-    /**
-     解析 计步睡眠数据 并保存Realm
-     */
-    func parseChartListData() {
-        
-        var startDate = ""
-        var endDate = ""
-        
-        if isNeedUpdateStepData() {
-            
-            let personalList = realm.objects(ChartStepDataRealm).filter("userId = '\(userId)'")
-            
-            if personalList.count != 0 {
-                
-                startDate = personalList.last!.time.toString(format: "yyyy-MM-dd HH:mm:ss")
-                endDate = NSDate().toString(format: "yyyy-MM-dd HH:mm:ss")
-                
-            }
-            
-            parseStepDate(startDate, endDate: endDate)
-            
-        }
-        
-        
-        if isNeedUpdateSleepData() {
-            
-            let personalList = realm.objects(ChartSleepDataRealm).filter("userId = '\(userId)'")
-            
-            if personalList.count != 0 {
-                
-                startDate = personalList.last!.time.toString(format: "yyyy-MM-dd HH:mm:ss")
-                endDate = NSDate().toString(format: "yyyy-MM-dd HH:mm:ss")
-                
-            }
-            
-            parseSleepDate(startDate, endDate: endDate)
-            
-        }
-        
- 
-    }
-    
-      /**
-     解析计步数据
-     时间格式： yyyy-MM-dd
-     有时间：更新数据
-     没有时间：解析全部数据
-     */
-    func parseStepDate(startDate: String, endDate: String) {
-
-        ChartsWebApi.shareApi.parseStepChartsData(startDate, endDate: endDate) { result in
-
-            guard result.isSuccess else {
-                Log.error("Parse Home Lists Data Error")
-                return
-            }
-            
-            do {
-                
-                let netResult = try ChartStepMsg(JSONDecoder(result.value!))
-
-                
-                for list in netResult.stepList {
-                    // 保存到数据库
-                    self.addStepListRealm(list)
-                }
-                
-            } catch let error {
-                
-                Log.error("\(#function) result error: \(error)")
-            }
-            
-        }
-
-    }
-    
-    /**
-     解析睡眠数据
-     时间格式： yyyy-MM-dd
-     有时间：更新数据
-     没有时间：解析全部数据
-     */
-    func parseSleepDate(startDate: String, endDate: String) {
-
-        ChartsWebApi.shareApi.parseSleepChartsData(startDate, endDate: endDate) { result in
-            
-            guard result.isSuccess else {
-                Log.error("Parse Home Lists Data Error")
-                return
-            }
-            do {
-                Log.info(result.value!)
-                let netResult = try ChartSleepMsg(JSONDecoder(result.value!))
-                for list in netResult.sleepList {
-                    
-                    Log.info(list)
-                    // 保存到数据库
-                    self.addSleepListRealm(list)
-                }
-                
-            } catch let error {
-                Log.error("\(#function) result error: \(error)")
-            }
-            
-        }
-
-    }
-    
-      /**
-     添加计步信息
-     
-     - author: sim cai
-     - date: 16-05-27 10:05:27
-     
-     
-     - parameter list: 计步信息JSON
-     */
-    func addStepListRealm(list: StepMsg) {
-        
-        guard let date = NSDate(fromString: list.dateTime, format: "yyyy-MM-dd HH:mm:ss") else {
-            return
-        }
-        
-        self.addStepData(ChartStepDataRealm(userId: self.userId, time: date, step: list.stepCount))
-
-    }
-    
-    func addSleepListRealm(list: SleepMsg) {
-        
-        guard let time = NSDate(fromString: list.dateTime, format: "yyyy-MM-dd HH:mm:ss") else {
-            Log.error("Time from erro [\(list.dateTime)]")
-            return
-        }
-        
-        self.addSleepData(ChartSleepDataRealm(userId: self.userId, time: time, tilts: list.rollCount))
-
-    }
-    
-    // MARK: 加载详情
+      // MARK: 加载详情
     /**
      显示计步页面
      */
